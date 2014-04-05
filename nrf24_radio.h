668b006aa8309136a33065a0579c2ad6e25f9fdd
jowood4 <jowood4@gmail.com>
2014-04-05 11:32:25 -0400

    Create nrf24_radio.h
    Initial commit for radio level header file for nRF24L01+ chip.

---
+++ nrf24_radio.h
@@ -0,0 +1,53 @@
+
+
+///////////////////////////////////////////////////
+
+
+/*
+Arduino based NRF24L01 Header File
+*/
+
+#ifndef WIRELESS_H
+#define WIRELESS_H
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include "NRF24L01_trinket.h"
+
+class Wireless{
+
+   public:
+	Wireless();
+	void setup(byte csn, byte ce);
+	byte receiver_mode(byte *rec_data);
+	void receiver_stop(void);
+	void receiver_start(void);
+	void transmitter_mode(byte *send_data, byte num_bytes);
+	byte get_status(void);
+	byte *address_array;
+	byte byteread;
+	nrf24L01_trinket radio;
+	byte *status_address;
+	byte status_data;
+
+   private:
+    	
+	byte data;
+	
+	
+	byte bytes_received;
+	byte current_FIFO;
+	byte temp;
+	byte sendByte[32];
+	byte transmit_address[5];
+	byte *trans_add;
+	byte read_byte;
+
+};
+
+
+#endif		
