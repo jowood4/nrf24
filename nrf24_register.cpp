a4860e5f1862cd09ae213a019137665f3a260292
jowood4 <jowood4@gmail.com>
2014-04-12 11:31:25 -0400

    Files now compiles in Arduino IDE.

--- nrf24_register.cpp
+++ nrf24_register.cpp
@@ -1,154 +1,141 @@
 #include "nrf24_register.h"
 
-//#include <SPI_trinket.h>
-
 nrf24_register::nrf24_register(){
 
 }
 
 void nrf24_register::setup(byte csn, byte ce){
-
- _CSN = csn;
- _CE = ce;
-
- pinMode(_CSN, OUTPUT);
- pinMode(_CE, OUTPUT);
-
- SPI.begin();
-
- digitalWrite(_CSN, 1);
- digitalWrite(_CE, 0);
+  	SPI_nrf.setup(csn, ce);
 }
 
 void nrf24_register::write_CSN(byte num){
- write_pin(_CSN, bit);
+ 	SPI_nrf.write_pin(_CSN, num);
  //digitalWrite(_CSN, num);
 }
 
 void nrf24_register::write_CE(byte num){
 
  //digitalWrite(_CE, num);
- write_pin(_CE, bit);
+ 	SPI_nrf.write_pin(_CE, num);
 }
 
 
 byte nrf24_register::read_register(byte address, unsigned char *status){
 
- byte data_read = 0;
+ 	byte data_read = 0;
 
- address &= 0x1F;
+ 	address &= 0x1F;
 
- write_pin(_CSN, 0);
+	 SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(address);
- data_read = spi_shift(0x00);
+ 	*status = SPI_nrf.spi_shift(address);
+ 	data_read = SPI_nrf.spi_shift(0x00);
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
 
- return data_read;
+ 	return data_read;
 }
 
 byte* nrf24_register::read_payload(byte num_bytes, byte *payload_address, byte *status){
 
- byte command = 0x61;
+ 	byte command = 0x61;
 
- write_pin(_CSN, 0);
+ 	SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(command);
- for(int j=0;j<num_bytes;j++){
-	 payload_address[j] = spi_shift(0);
-}
+ 	*status = spi_shift(command);
+ 	for(int j=0;j<num_bytes;j++){
+	 	payload_address[j] = spi_shift(0);
+	}
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
 
- return payload_address;
+ 	return payload_address;
 }
 
 
 void nrf24_register::write_register(byte address, byte data, byte *status){
 
- address &= 0x1F;
- address += 0x20;
+ 	address &= 0x1F;
+ 	address += 0x20;
 
- write_pin(_CSN, 0);
+ 	SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(address);
-  spi_shift(data);
+ 	*status = spi_shift(address);
+  	SPI_nrf.spi_shift(data);
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
  
-
 }
 
 void nrf24_register::set_rw_address(byte address, byte *data, byte num_bytes, byte *status){
 
- address &= 0x1F;
- address += 0x20;
+	address &= 0x1F;
+ 	address += 0x20;
 
- write_pin(_CSN, 0);
+ 	SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(address);
+ 	*status = spi_shift(address);
 
- for(int i = 0; i<num_bytes; i++){
- spi_shift(*data++);
-}
+ 	for(int i = 0; i<num_bytes; i++){
+ 		SPI_nrf.spi_shift(*data++);
+	}
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
 
 }
 
 byte* nrf24_register::read_rw_address(byte address, byte *address_address, byte *status){
 
- address &= 0x1F;
+ 	address &= 0x1F;
 
- write_pin(_CSN, 0);
+ 	SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(address);
+ 	*status = spi_shift(address);
 
- for(int i = 0; i<5; i++){
-   address_address[i] = spi_shift(0x00);
-}
+ 	for(int i = 0; i<5; i++){
+   		address_address[i] = spi_shift(0x00);
+	}
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
 
- return address_address;
+ 	return address_address;
 }
 
 
 void nrf24_register::write_payload(byte *data_write, byte num_bytes, byte *status){
 
- byte command = 0xA0;
+ 	byte command = 0xA0;
 
- write_pin(_CSN, 0);
+ 	SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(command);
+ 	*status = spi_shift(command);
 
- for(int j=0;j<num_bytes;j++){
- spi_shift(*data_write++);
-}
+ 	for(int j=0;j<num_bytes;j++){
+ 		spi_shift(*data_write++);
+	}
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
 
 }
 
 void nrf24_register::send_command(byte command, byte *status){
 
- write_pin(_CSN, 0);
+ 	SPI_nrf.write_pin(_CSN, 0);
 
- *status = spi_shift(command);
+ 	*status = SPI_nrf.spi_shift(command);
 
- write_pin(_CSN, 1);
+ 	SPI_nrf.write_pin(_CSN, 1);
 
 }
 
 byte nrf24_register::spi_shift(byte data_in){
 
- byte data_out;
+ 	byte data_out;
 
- //data_out = SPI.transfer(data_in);
- data_out = SPI.spi_shift(data_in);
+ //data_out = SPI_nrf.transfer(data_in);
+ 	data_out = SPI_nrf.spi_shift(data_in);
 
- return data_out;
+ 	return data_out;
 }
 
 void nrf24_register::write_buffer(byte address, byte *buffer, byte num_bytes, byte *status){
@@ -158,15 +145,19 @@ void nrf24_register::write_buffer(byte address, byte *buffer, byte num_bytes, by
 	address &= 0x1F;
 	address += 0x20;
 
-	write_pin(_CSN, 0);
+	SPI_nrf.write_pin(_CSN, 0);
 
 	*status = spi_shift(address);
 
 	for(i = 0; i<num_bytes; i++){
-		spi_shift(buffer[i]);
+		SPI_nrf.spi_shift(buffer[i]);
 	}
 
-	write_pin(_CSN, 1);
+	SPI_nrf.write_pin(_CSN, 1);
+}
+
+byte nrf24_register::read_IRQ(void){
+	return SPI_nrf.read_pin(_IRQ);
 }
 
 ////////////////////////////////////////////////////////
