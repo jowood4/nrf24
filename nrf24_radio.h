156b526585aeb3a9704977c2051ea0b172933150
jowood4 <jowood4@gmail.com>
2014-04-12 11:28:26 -0400

    Files now compiles in Arduino IDE.

--- nrf24_radio.h
+++ nrf24_radio.h
@@ -1,44 +1,39 @@
 #ifndef NRF24_RADIO_H
 #define NRF24_RADIO_H
 
-#ifdef TRINKET
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-#include <NRF24L01_trinket.h>
-
-#elif ARDUINO
-#include <NRF24L01_arduino.h>
-
-#else
-#include <NRF24L01_arduino.h>
-#endif
+#include "nrf24_register.h"
 
 class nrf24_radio{
 
    public:
-	Wireless();
+	nrf24_radio();
 	void setup(byte csn, byte ce);
 	byte receiver_mode(byte *rec_data);
 	void receiver_stop(void);
 	void receiver_start(void);
 	void transmitter_mode(byte *send_data, byte num_bytes);
 	byte get_status(void);
+	byte setup_retransmit(byte data);
+	byte address_width(byte data);
+	byte enable_RX_addr(byte data);
+	byte auto_acknowledge(byte data);
+	byte flush_TX_buffer(void);
+	byte flush_RX_buffer(void);
+	byte bytes_received(void);
+	byte setup_frequency(byte data);
+
 	byte *address_array;
 	byte byteread;
-	nrf24L01_trinket radio;
+	nrf24_register radio;
 	byte *status_address;
 	byte status_data;
 
    private:
-    	
 	byte data;
 	byte frequency;
 	byte retransmit;
 	byte use_IRQ;
-	byte bytes_received;
+	byte received_bytes;
 	byte current_FIFO;
 	byte sendByte[32];
 	byte transmit_address[5];
@@ -46,6 +41,5 @@ class nrf24_radio{
 	byte read_byte;
 
 };
-
 
 #endif		
