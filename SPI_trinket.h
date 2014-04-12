e0fc79ed57deb553327654faf91eea5c9b86a2d8
jowood4 <jowood4@gmail.com>
2014-04-12 11:26:39 -0400

    Files now compiles in Arduino IDE.

--- SPI_trinket.h
+++ SPI_trinket.h
@@ -1,19 +1,31 @@
 #ifndef SPI_trinket_h
 #define SPI_trinket_h
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#define CLOCK 2
+#define DWRITE 0
+#define DREAD 1
+
 class SPI_trinket
 {
 public:
-      void setup(int clk, int dwr, int dre);
-      int spi_shift(int data_write);
-	    void pin_write(int pin, int val);
+      	void setup(byte csn, byte ce);
+      	int spi_shift(int data_write);
+	void write_pin(int pin, int val);
+	int read_pin(int pin);
 	    
 private:
-      byte CLOCK;
-      byte DWRITE;
-      byte DREAD;
+      	byte _CE;
+      	byte _CSN;
+      	byte _CLK;    
+      	byte _MOSI;
+      	byte _MISO;
+	byte _IRQ;
 };
-
-extern SPI_trinket SPI;
 
 #endif
