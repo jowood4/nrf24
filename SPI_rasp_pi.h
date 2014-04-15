131f4ac970f86cf9d1973252eb5a9bd1b3b0e9fe
jowood4 <jowood4@gmail.com>
2014-04-14 21:53:36 -0400

    Create SPI_rasp_pi.h

---
+++ SPI_rasp_pi.h
@@ -0,0 +1,23 @@
+#ifndef SPI_rasp_pi_h
+#define SPI_rasp_pi_h
+
+#include <wiringPi.h>
+
+class SPI_rasp_pi
+{
+public:
+      void setup(int csn, int ce);
+      int spi_shift(int data_write);
+	    void write_pin(int pin, int val);
+	    int read_pin(int pin);
+
+private:
+      int _CE;
+      int _CSN;
+      int _CLK;    
+      int _MOSI;
+      int _MISO;
+	    int _IRQ;
+};
+
+#endif
